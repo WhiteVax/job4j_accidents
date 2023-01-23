d01bb668acf94746c144bd946ca3465a96004b3a
WhiteVax <WhiteVax@gmail.com>
2023-01-23 19:18:23 +0200

    0. Spring DataSource.[edit]

--- src/main/java/ru/accidents/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/accidents/repository/AccidentJdbcTemplate.java
@@ -3,10 +3,13 @@ package ru.accidents.repository;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.accidents.model.Accident;
 import ru.accidents.model.AccidentType;
 
+import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,23 +17,36 @@ import java.util.Optional;
 @AllArgsConstructor
 public class AccidentJdbcTemplate {
     private static final String INSERT_ACCIDENT = "INSERT INTO accident (name, description, address, created, type_id) VALUES (?, ?, ?, ?, ?)";
-    private static final String SELECT_ALL_ACCIDENT = "SELECT * FROM accident ORDER BY ID";
+    private static final String SELECT_ALL_ACCIDENT =
+                    """
+                    SELECT a.id, a.name, a.description, a.address, a.type_id, tp.name type, a.created 
+                    FROM accident a 
+                    JOIN accident_type tp ON a.type_id = tp.id 
+                    ORDER BY a.id
+                    """;
+
     private static final String SELECT_GET_ID_ACCIDENT = "SELECT * FROM accident WHERE id = ?";
     private static final String UPDATE_ACCIDENT = "UPDATE accident SET name = ?, description =?, address = ?, type_id = ? WHERE id = ?";
     private static final String DELETE_ACCIDENT = "DELETE FROM accident WHERE id = ?";
-    private static final String SELECT_BY_ID_ACCIDENT_TYPE = "SELECT * FROM accident_type WHERE id = ?";
     private static final String DELETE_BY_ACCIDENT_ID_ACCIDENTS_RULES = "DELETE FROM accidents_rules WHERE accident_id = ?";
     private static final String INSERT_ACCIDENTS_RULES = "INSERT INTO accidents_rules(accident_id, rule_id) VALUES (?, ?)";
 
     private final JdbcTemplate jdbc;
 
     public Accident create(Accident accident) {
-        jdbc.update(INSERT_ACCIDENT,
-                accident.getName(),
-                accident.getDescription(),
-                accident.getAddress(),
-                accident.getCreated(),
-                accident.getType().getId());
+        var keyHolder = new GeneratedKeyHolder();
+        jdbc.update(connection -> {
+            var ps = connection.prepareStatement(INSERT_ACCIDENT, new String[]{"id"});
+            ps.setString(1, accident.getName());
+            ps.setString(2, accident.getDescription());
+            ps.setString(3, accident.getAddress());
+            ps.setTimestamp(4, Timestamp.valueOf(accident.getCreated()));
+            ps.setInt(5, accident.getType().getId());
+            return ps;
+        }, keyHolder);
+        for (var rule : accident.getRules()) {
+            insertRulesAndAccidents(keyHolder.getKey().intValue(), rule.getId());
+        }
         return accident;
     }
 
@@ -41,10 +57,9 @@ public class AccidentJdbcTemplate {
                     accident.setId(rs.getInt("id"));
                     accident.setName(rs.getString("name"));
                     accident.setDescription(rs.getString("description"));
-                    accident.setType(jdbc.queryForObject(
-                            SELECT_BY_ID_ACCIDENT_TYPE, new BeanPropertyRowMapper<>(AccidentType.class), rs.getInt("type_id")));
+                    accident.setType(typeRowMapper.mapRow(rs, row));
                     accident.setCreated(rs.getTimestamp("created").toLocalDateTime());
-                    accident.setAddress("address");
+                    accident.setAddress(rs.getString("address"));
                     return accident;
                 });
     }
@@ -55,15 +70,15 @@ public class AccidentJdbcTemplate {
 
     public void update(Accident accident) {
         deleteRulesAndAccidents(accident.getId());
-        for (var rule: accident.getRules()) {
-            insertRulesAndAccidents(accident.getId(), rule.getId());
-        }
         jdbc.update(UPDATE_ACCIDENT,
                 accident.getName(),
                 accident.getDescription(),
                 accident.getAddress(),
                 accident.getType().getId(),
                 accident.getId());
+        for (var rule : accident.getRules()) {
+            insertRulesAndAccidents(accident.getId(), rule.getId());
+        }
     }
 
     public void delete(int id) {
@@ -78,4 +93,11 @@ public class AccidentJdbcTemplate {
     public void deleteRulesAndAccidents(int id) {
         jdbc.update(DELETE_BY_ACCIDENT_ID_ACCIDENTS_RULES, id);
     }
+
+    private final RowMapper<AccidentType> typeRowMapper = (resultSet, rowNum) -> {
+        var type = new AccidentType();
+        type.setId(resultSet.getInt("type_id"));
+        type.setName(resultSet.getString("type"));
+        return type;
+    };
 }
